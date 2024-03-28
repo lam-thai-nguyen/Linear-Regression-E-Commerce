529ed60fc060475985532d28bf22054e7c43562a
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-03-28 14:45:05 +0700

    add z-normalize function

--- utils.py
+++ utils.py
@@ -20,3 +20,20 @@ def plot_model(features: np.array, targets: np.array, w: float, b: float) -> Non
     plt.plot(features, w * features + b, c='r')
     plt.show()
     
+    
+def z_normalize(X: np.array):
+    """
+    After z-score normalization, all features will have a mean of 0 and a standard deviation of 1.
+    
+    Argument: X: features
+    Returns: normalized_X
+    """
+    # Feature mean
+    mu = np.mean(X, axis=0)
+    # Feature standard deviation
+    std = np.std(X, axis=0)
+    
+    X_norm = (X - mu) / std
+    
+    return X_norm
+    
